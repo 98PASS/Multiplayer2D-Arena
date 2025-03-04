dad49e4e234decce9927ad062e3495383f973a51
Pedro <panssfancy@gmail.com>
2025-03-04 11:52:24 -0300

    Adição do player com comandos básicos de movimentação 360°

---
+++ Player/player_2d.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	var direction := Vector2 (Input.get_axis("ui_left", "ui_right"),Input.get_axis('ui_up','ui_down'))
+	velocity = direction * SPEED
+	move_and_slide()
